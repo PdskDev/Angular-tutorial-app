a8e81bfa5c7193ebeb62cebeaeeff116ee0cb22c
NadetDev <nadetdev@gmail.com>
2024-04-22 21:09:23 +0200

    Basic Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'tutorial-angular-01';
+  welcomeMessage: string = "Bonjour les amis";
+  inputMessage: string = "Merci de saisir un texte ici";
+  colSpanValue: number = 3;
+  messageAlerte: string = "Pourquoi tu cliques sur moi ?";
+
+  showMessage(messageAlerte: string){
+    alert(this.messageAlerte);
+  }
+
+  showMessageOnKeyPress(onKeyPressEvent: any){
+    this.messageAlerte = onKeyPressEvent.target?.value;
+    this.showMessage(this.messageAlerte);
+  }
+
 }
