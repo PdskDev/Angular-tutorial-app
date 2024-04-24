e46efded1388fd5b548f684a8ff82eff473e4167
NadetDev <nadetdev@gmail.com>
2024-04-24 21:05:43 +0200

    Book Details Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
-import { BookListComponent } from './books/book-list/book-list.component';
 import { BookHomeComponent } from './books/book-home/book-home.component';
-import { RouterModule } from '@angular/router';
 import { AppHomeComponent } from './home/app-home/app-home.component';
+import { BookDetailsComponent } from './books/book-details/book-details/book-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BookListComponent,
     BookHomeComponent,
-    AppHomeComponent
+    AppHomeComponent,
+    BookDetailsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/books/book-details/book-details/book-details.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+import { Book } from 'src/app/models/Book';
+
+@Component({
+  selector: 'app-book-details',
+  templateUrl: './book-details.component.html',
+  styleUrls: ['./book-details.component.css']
+})
+export class BookDetailsComponent {
+  @Input() book: Book | undefined;
+
+
+
+}
