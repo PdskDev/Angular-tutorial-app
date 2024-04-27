ce677574a9b23bd03264a632c5b24f26f4939574
NadetDev <nadetdev@gmail.com>
2024-04-27 15:48:37 +0200

    Book search suggestions

--- src/app/books/book-search/book-search.component.ts
+++ src/app/books/book-search/book-search.component.ts
@@ -1,5 +1,5 @@
-import { Component, ElementRef, OnInit, ViewChild,  } from '@angular/core';
-import { Observable, Subject, Subscription, debounceTime, delay, distinctUntilChanged, fromEvent, map, mergeMap, of, tap } from 'rxjs';
+import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild,  } from '@angular/core';
+import { Observable, Subject, Subscription, debounceTime, delay, distinctUntilChanged, fromEvent, map, mergeMap, of, switchMap, tap } from 'rxjs';
 import { BookServiceService } from 'src/app/services/book-service.service';
 
 @Component({
@@ -8,26 +8,67 @@ import { BookServiceService } from 'src/app/services/book-service.service';
   styleUrls: ['./book-search.component.css']
 })
 export class BookSearchComponent implements OnInit {
-  @ViewChild('searchInput') searchInput: ElementRef | undefined;
-  bookTitles: Array<string> | undefined;
 
-  public keyUP = new Subject<KeyboardEvent>();
-  private subscription: Subscription | undefined;
+  @ViewChild('searchInput') searchInput: ElementRef | undefined;
+  @ViewChild('suggestions') suggestions: ElementRef | undefined;
+  bookTitles: Array<string> = [];
+  searchInputTerm: string = '';
+
+  @Output() searchBookTitle = new EventEmitter<string>();
+
+  public searchTitleKeyUP = new Subject<KeyboardEvent>();
+  public sugessionsClick = new Subject<MouseEvent>();
+  private subscriptionInputSearch: Subscription | undefined;
+  private subscriptionSugessionsClick: Subscription | undefined;
 
   constructor(private bookStoreService: BookServiceService){
 
   }
 
   ngOnInit(): void {
-   this.subscription = this.keyUP.pipe(
+   /* this.subscriptionInputSearch = this.keyUP.pipe(
       map(event => (<HTMLInputElement>event.target).value),
       debounceTime(300),
       distinctUntilChanged(),
       mergeMap(search => of(search).pipe(delay(200))),
     ).subscribe(title =>
       this.bookStoreService.getOnlyTitlesOfBooks(title)
-      .subscribe(bookTitles => this.bookTitles = bookTitles));
+      .subscribe(bookTitles => this.bookTitles = bookTitles)); */
+
+      this.booksSearch();
+      this.clickTitleSuggestions();
   }
+
+  searchBooks() {
+    this.bookTitles = [];
+    this.searchBookTitle.emit(this.searchInputTerm);
+
+  }
+
+  booksSearch(){
+    const search$ = this.searchTitleKeyUP.pipe(
+      map((event: KeyboardEvent) => (<HTMLInputElement>event.target).value),
+      debounceTime(400),
+      distinctUntilChanged(),
+      switchMap((title) => this.bookStoreService.getOnlyTitlesOfBooks(title))
+    );
+
+    search$.subscribe(booksTitlesList => this.bookTitles = booksTitlesList);
+  }
+
+  clickTitleSuggestions(){
+    const search$ = this.sugessionsClick.pipe(
+      map((event: MouseEvent) => (<HTMLInputElement>event.target).innerText),
+      debounceTime(100),
+      distinctUntilChanged(),
+    );
+
+    search$.subscribe(title => {
+      this.searchInputTerm = title;
+      this.bookTitles = [];
+    });
+  }
+
 
 }
 
