e9a69a0c6a34c4f5d6ec6ac7467535edcd7b5360
NadetDev <nadetdev@gmail.com>
2024-04-28 17:22:34 +0200

    Template driven form validations

--- src/app/forms/registration-form/registration-form.component.ts
+++ src/app/forms/registration-form/registration-form.component.ts
@@ -11,6 +11,8 @@ interface User {
   styleUrls: ['./registration-form.component.css']
 })
 export class RegistrationFormComponent {
+  EMAIL_REGEX = "^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$";
+  PASSWORD_STRENGTH_REGEX = "(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.*[a-z])(?=.*[A-Z])[a-zA-Z0-9!@#\$%\^&\*]{8,}";
 
   user: User = {
     firstName: 'Nadet',
