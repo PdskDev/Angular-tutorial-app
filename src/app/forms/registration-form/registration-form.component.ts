28e3a7fbd94f51b03f0842445805ad87127365bb
NadetDev <nadetdev@gmail.com>
2024-04-28 16:24:12 +0200

    Form validation

--- src/app/forms/registration-form/registration-form.component.ts
+++ src/app/forms/registration-form/registration-form.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
 
+interface User {
+  firstName: string,
+  lastName: string
+}
+
 @Component({
   selector: 'app-registration-form',
   templateUrl: './registration-form.component.html',
   styleUrls: ['./registration-form.component.css']
 })
 export class RegistrationFormComponent {
+
+  user: User = {
+    firstName: 'Nadet',
+    lastName: 'Dev'
+  }
+
+  onSubmit(formValue: any){
+    console.log(formValue)
+  }
+
 
 }
